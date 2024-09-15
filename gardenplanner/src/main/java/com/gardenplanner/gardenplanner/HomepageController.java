2adf743f4590da6dec945904a5c8da321a5332f2
Siah Chun Sheng <johnson7797@gmail.com>
2024-09-15 19:57:40 +1000

    Updated logout function in homepage

--- gardenplanner/src/main/java/com/gardenplanner/gardenplanner/HomepageController.java
+++ gardenplanner/src/main/java/com/gardenplanner/gardenplanner/HomepageController.java
@@ -1,7 +1,18 @@
 package com.gardenplanner.gardenplanner;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Optional;
 
 public class HomepageController {
 
@@ -20,6 +31,27 @@ public class HomepageController {
     @FXML
     void handleButtonClick() {
 
+    }
+
+    @FXML
+    void handleLogOut() throws IOException {
+        ButtonType YesButton = new ButtonType("Yes");
+        ButtonType CancelButton = new ButtonType("Cancel");
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you sure you want to logout?", YesButton, CancelButton);
+        alert.setTitle("Log out");
+        alert.setHeaderText("Log out");
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.isPresent()) {
+            if(result.get() == YesButton) {
+                Parent loginPage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/gardenplanner/gardenplanner/loginpage.fxml")));
+                Stage stage = (Stage) logOutButton.getScene().getWindow();
+                stage.setScene(new Scene(loginPage, 900, 600));
+                stage.show();
+            }
+            else {
+                alert.close();
+            }
+        }
     }
 
 
