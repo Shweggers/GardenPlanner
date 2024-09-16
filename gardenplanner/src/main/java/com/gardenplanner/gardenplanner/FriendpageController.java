b59b08dbaf169a9687ea45a054103c75a47fba39
Harry Golding <harrygolding01@gmail.com>
2024-09-16 15:45:12 +1000

    added functionality to exit friendpage back to homepage

--- gardenplanner/src/main/java/com/gardenplanner/gardenplanner/FriendpageController.java
+++ gardenplanner/src/main/java/com/gardenplanner/gardenplanner/FriendpageController.java
@@ -1,8 +1,27 @@
 package com.gardenplanner.gardenplanner;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 
 public class FriendpageController {
 
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    void backHome() throws IOException {
+        Parent homepage = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/gardenplanner/gardenplanner/homepage.fxml")));
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.setScene(new Scene(homepage, 800, 600));
+        stage.show();
+
+    }
 }
