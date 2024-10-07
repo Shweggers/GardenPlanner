2c9996ba1cf65b65dd1f0444d5342711aae29127
Sweg <62927898+Shweggers@users.noreply.github.com>
2024-10-08 08:55:24 +1000

    Create DatabaseTest.java

---
+++ gardenplanner/src/test/java/DatabaseTest.java
@@ -0,0 +1,14 @@
+import com.gardenplanner.gardenplanner.model.DatabaseConnection;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class DatabaseTest {
+    @Test
+    public void testConnection() {
+        Connection conn = DatabaseConnection.getInstance();
+        assertNotNull(conn);
+    }
+}
