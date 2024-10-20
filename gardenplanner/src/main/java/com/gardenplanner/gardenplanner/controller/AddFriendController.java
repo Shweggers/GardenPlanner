c0dd157a4275c641dafcf005ce8304c06dc4515c
Sweg <62927898+Shweggers@users.noreply.github.com>
2024-10-20 16:24:19 +1000

    fix controller

--- gardenplanner/src/main/java/com/gardenplanner/gardenplanner/controller/AddFriendController.java
+++ gardenplanner/src/main/java/com/gardenplanner/gardenplanner/controller/AddFriendController.java
@@ -1,17 +1,13 @@
 // Java
 package com.gardenplanner.gardenplanner.controller;
 
-import com.gardenplanner.gardenplanner.model.DataStore;
-import com.gardenplanner.gardenplanner.model.UserManager;
-import com.gardenplanner.gardenplanner.model.DAO.IUserDAO;
+import com.gardenplanner.gardenplanner.model.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-
-import javax.sql.RowSet;
 
 /**
  * AddFriendController is a class that represents a controller for adding a new friend.
@@ -24,13 +20,10 @@ public class AddFriendController {
     @FXML
     private Button cancelButton;
 
-    private final FriendManager friendManager;
-
     /**
      * Constructs a new AddFriendController
      */
     public AddFriendController() {
-        friendManager = FriendManager.getInstance();
     }
 
     /**
@@ -42,21 +35,18 @@ public class AddFriendController {
     public void confirmButtonClicked(ActionEvent event) {
         String friendName = addFriendName.getText();
         if (friendName != null && !friendName.trim().isEmpty()) {
-            UserManager userManager = UserManager.getInstance();
-            RowSet currentUser;
-            int userId = userManager.returnID(currentUser.getUsername());
-            int friendId = userManager.returnID(friendName);
+            User user = DataStore.getInstance().getCurrentUser();
+            User friend = UserManager.getInstance().getUser(friendName);
 
             // Check if the friend exists in the database
-            User friend = userManager.getUser(friendName);
             if (friend == null) {
                 new Alert(Alert.AlertType.ERROR, "Friend not found!").showAndWait();
                 return;
             }
 
             // Create a new Friend object
-            Friend newFriend = new Friend(userId, friendId);
-            friendManager.addFriend(newFriend);
+            Friend newFriend = new Friend(user.ID(), friend.ID(), friendName);
+            FriendManager.getInstance().insert(newFriend);
 
             // Close the current window
             Stage stage = (Stage) confirmButton.getScene().getWindow();
